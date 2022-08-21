16fff538e8f343f770215cf189a8123f5776a1cc
Julian.Chetty <julchetty@live.com>
2022-08-21 15:54:52 +0400

    add error handling middleware

---
+++ Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,34 @@
+// add error handling middleware
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace Middleware
+{
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public ErrorHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            return context.Response.WriteAsync(text: new ProblemDetails() { Title = "An error occurred while processing your request.", Detail = exception.Message }.ToString());
+        }
+    }
+}
