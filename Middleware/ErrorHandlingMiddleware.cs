d405888350ff4e3521aa5991c36534eef18c565e
Julian.Chetty <julchetty@live.com>
2022-08-22 17:55:25 +0400

    refactor exception handling

--- Middleware/ErrorHandlingMiddleware.cs
+++ Middleware/ErrorHandlingMiddleware.cs
@@ -1,3 +1,5 @@
+using System.Net;
+using System.Text.Json;
 // add error handling middleware
 
 using Microsoft.AspNetCore.Mvc;
@@ -22,11 +24,18 @@ namespace minimalapicleanarchitecture.Middleware
                 await HandleExceptionAsync(context, ex);
             }
         }
-        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        private static async Task HandleExceptionAsync(HttpContext context, Exception exception)
         {
-            context.Response.ContentType = "application/json";
-            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
-            return context.Response.WriteAsync(text: new ProblemDetails { Title = "An error occurred while processing your request.", Detail = exception.Message }.ToString()!);
+             var response = context.Response;
+            response.ContentType = "application/json";
+            response.StatusCode = exception switch
+            {
+                ApplicationException => (int)HttpStatusCode.BadRequest,// custom application error
+                KeyNotFoundException  => (int)HttpStatusCode.NotFound,// not found error
+                _ => (int)HttpStatusCode.InternalServerError,// unhandled error
+            };
+            var result = JsonSerializer.Serialize(new { message = exception?.Message });
+            await response.WriteAsync(result);
         }
     }
 }
